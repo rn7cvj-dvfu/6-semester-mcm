99bb3f1b5d59679ea86f4d33f214858b0813b0b1
rn7cvj-dvfu <rn7cvj-students@outlook.com>
2025-06-25 16:58:25 +1000

    fix

--- mcm_site/lib/model.dart
+++
@@ -1,7 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-@freezed
-class ParameterModel with _$ParameterModel {
-  factory ParameterModel.value() = _ParameterModelValue;
-  factory ParameterModel.toggle() = _ParameterModelToggle;
-}
